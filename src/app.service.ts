0d88337558c299c674459329c5c934724afc885c
d4tzz <d4tzz@temp.com>
2025-12-06 17:51:21 +0700

    try deploy again

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Docker, Azure Container App !!! test the speed of deploy. Confirmation';
+    return 'Hello Nestjs project deployment';
   }
 }
