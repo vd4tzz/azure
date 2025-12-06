0bae13145d86abbd78449fce8c74998cdba9e738
d4tzz <d4tzz@temp.com>
2025-12-06 16:46:34 +0700

    confirmation

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Docker, Azure Container App !!! test the speed of deploy';
+    return 'Hello Docker, Azure Container App !!! test the speed of deploy. Confirmation';
   }
 }
